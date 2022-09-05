b147ef9944d84b87c89108ab98f19e111f2d922a
RunyShark <92100746+RunyShark@users.noreply.github.com>
2022-09-04 21:23:50 -0500

    test

--- src/api/user.controller.spec.ts
+++ src/api/user.controller.spec.ts
@@ -3,9 +3,12 @@ import { UserController } from './user.controller';
 import { User } from './entitis/user.entiti';
 import { getModelToken } from '@nestjs/mongoose';
 import { UserModule } from './user.module';
+import { UserSerializer } from './serializer/UserSerializer';
+import { UserService } from './user.service';
 
 describe('UserController', () => {
   let controller: UserController;
+  let userService: UserService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -16,9 +19,25 @@ describe('UserController', () => {
       .compile();
 
     controller = module.get<UserController>(UserController);
+    userService = module.get<UserService>(UserService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('getUser', () => {
+    it('should be defined', async () => {
+      jest
+        .spyOn(userService, 'getUser')
+        .mockImplementation(
+          () =>
+            Promise.resolve([{ name: 'example' }]) as unknown as Promise<
+              User[]
+            >,
+        );
+
+      const result = await controller.getUser();
+
+      expect(result).toHaveLength(1);
+      expect(result[0] instanceof UserSerializer).toEqual(true);
+      expect(userService.getUser).toHaveBeenCalledTimes(1);
+    });
   });
 });
